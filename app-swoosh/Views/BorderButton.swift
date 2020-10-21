4ae136abd21cdc3011bd90d0569a9173c2a4f99a
Adler <adler@ese.plus>
2020-10-21 11:52:07 -0600

    Added a comment on why I did it to make it work

--- app-swoosh/Views/BorderButton.swift
+++ app-swoosh/Views/BorderButton.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class BorderButton: UIButton {
 
+    //Had to delete class name on the override
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.borderWidth = 2.0
